5bd0cb7f9e1f4c272e236bd45f7a922ed91ceeb3
dvader33 <vhader33.dr@gmail.com>
2018-04-04 16:56:40 -0600

    inital commit

---
+++ src/components/home/home.vue
@@ -0,0 +1,16 @@
+<template src="./splash.html"></template>
+<style lang="scss" scoped src="./splash.scss"></style>
+
+<script>
+import facebook from '@/assets/svg/facebook.svg';
+import twitter from '@/assets/svg/twitter.svg';
+import behance from '@/assets/svg/behance.svg';
+import github from '@/assets/svg/github.svg';
+import youtube from '@/assets/svg/youtube.svg';
+
+export default {
+  components: {
+    // Particles
+  }
+}
+</script>
